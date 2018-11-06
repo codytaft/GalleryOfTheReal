46835d8e78b693b9368f714368372389a58fd102
Cody Taft <cody.taft@gmail.com>
2018-11-06 16:42:40 -0700

    Begin image container buildout

---
+++ src/components/ImageContainer.vue
@@ -0,0 +1,21 @@
+<template>
+<div id="image-container">
+  <ul class="imageContainer">
+    {{ objects }}
+  </ul>
+
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'image-container',
+  props:  {
+    objects: Array
+  }
+}
+</script>
+
+<style scoped>
+</style>
