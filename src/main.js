076520b1d6eb5f65421dd454934ee049a5386c62
Cody Taft <cody.taft@gmail.com>
2018-11-06 16:41:29 -0700

    Remove default url from axio

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
-import axios from 'axios';
+// import axios from 'axios';
 
-axios.defaults.baseURL = 'https://api.harvardartmuseums.org';
+// axios.defaults.baseURL = 'https://api.harvardartmuseums.org';
 
 Vue.config.productionTip = false;
 
