f68c0c4e807b8f05dafe5b154e308716e1f32b7c
Cody Taft <cody.taft@gmail.com>
2018-11-06 16:41:15 -0700

    Seprate getch and cleaner methods in app

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,10 @@
   <div id="app">
     <Header msg="Welcome to the Gallery of the Real"/>
     <hr>
-    <button class="pill-btn blue-pill"></button>
-    <button class="pill-btn red-pill"></button>
+    <div>
+      <ImageContainer v-bind:objects="this.images"/>
+    </div>
+    <button v-on:click="getAllObjects()"></button>
   </div>
 </template>
 
@@ -12,31 +14,67 @@
 
 <script>
 import Header from './components/Header.vue'
+import ImageContainer from './components/ImageContainer.vue'
 import GetGallery from './services/GetGallery'
-
+import apiKey from './assets/apiKey';
+import axios from 'axios';
 
 export default {
   name: 'app',
   components: {
-    Header
+    Header,
+    ImageContainer
   },
   data() {
     return {
+      home: true,
       loading: true,
-      periods: []
+      objects: [],
+      images: [],
+      nextLink: ''
+    }
+  },
+  methods: {
+
+    getAllObjects(url=`https://api.harvardartmuseums.org/object?hasimage=1&apikey=${apiKey}`) {
+      console.log(url)
+      return axios.get(url)
+        .then(response => {
+          this.getImages(response.data)
+        })
+        .catch(error => console.log(error))
+        .finally(() => {
+          this.loading = false
+        })
+    },
+
+    getImages: function(objects) {
+      console.log(objects)
+      return objects.records.forEach(object => {
+        console.log(object)
+        if (object.primaryimageurl !== null && this.images.length < 10 ) {
+          this.images.push(object.primaryimageurl)
+        } else if(this.images.length < 10) {
+          this.getAllObjects(objects.info.next)
+        } else if(this.images.length === 10) {
+          this.nextLink = objects.info.next
+        } 
+        return this.images.push(object.primaryimageurl)
+      })
     }
   },
 
-  created() {
-    GetGallery.getPeriod()
-    .then(periods => {
-      this.periods = periods.records
-    })
-    .catch(error => console.log(error))
-    .finally(() => {
-      this.loading = false
-    })
-  }
+  // created() {
+  //   const url = `https://api.harvardartmuseums.org/object?hasimage=1&apikey=${apiKey}`
+  //   GetGallery.getAllObjects(url)
+  //   .then(objects => {
+  //     this.getImages(objects)
+  //   })
+  //   .catch(error => console.log(error))
+  //   .finally(() => {
+  //     this.loading = false
+  //   })
+  // }
 }
 </script>
 
